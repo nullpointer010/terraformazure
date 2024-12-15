fca3dd78899993f464faa4b87b2c7e5eb43c4444
nullpointer010 <49768012+nullpointer010@users.noreply.github.com>
2024-12-16 00:50:34 +0100

    Update variables.tf

--- modules/Google-Storage/variables.tf
+++ modules/Google-Storage/variables.tf
@@ -5,15 +5,15 @@
 
 variable "gcp_username" {
   description = "El nombre de usuario en Google Cloud Platform (GCP)."
-  default     = "" 
+  default     = "mk3500" 
 }
 
 variable "gcp_project" {
   description = "El ID del proyecto en Google Cloud Platform (GCP)."
-  default     = "" 
+  default     = "mk3500" 
 }
 
 variable "gcp_bucket_name" {
   description = "El nombre del bucket a crear en Google Storage."
-  default     = ""
+  default     = "imagen_buckets"
 }
