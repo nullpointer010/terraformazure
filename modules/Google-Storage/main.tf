7e7c75a50e5566e2311e56d76a66d14dfaa0dc5e
nullpointer010 <49768012+nullpointer010@users.noreply.github.com>
2024-12-15 23:58:15 +0100

    Update main.tf

--- modules/Google-Storage/main.tf
+++ modules/Google-Storage/main.tf
@@ -1,4 +1,3 @@
-# *** YOUR CODE HERE ***
 # Crear un recurso google_storage_bucket para almacenar imágenes en Google Storage
 #
 # Crear en el bucket anteior dos recursos google_storage_bucket_object
@@ -9,4 +8,44 @@
 # Configurar google_storage_object_access_control así
 #   role   = "READER"
 #   entity = "allUsers"
-# **********************  
+
+resource "google_storage_bucket" "image_bucket" {
+  name     = var.gcp_bucket_name
+  location = "US"                  # ver regiones
+  storage_class = "STANDARD"       
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 365                    # Elimina objetos después de un año
+    }
+  }
+}
+
+resource "google_storage_bucket_object" "image1" {
+  name   = "el_enigma_de_la_habitacion_622.jpg"
+  bucket = google_storage_bucket.image_bucket.name
+  source = "./images/el_enigma_de_la_habitacion_622.jpg"  # Ruta 
+}
+
+resource "google_storage_bucket_object" "image2" {
+  name   = "una_historia_de_espana.jpg"
+  bucket = google_storage_bucket.image_bucket.name
+  source = "./images/una_historia_de_espana.jpg"  # Ruta
+}
+
+resource "google_storage_object_access_control" "image1_access" {
+  bucket = google_storage_bucket.image_bucket.name
+  object = google_storage_bucket_object.image1.name
+  role   = "READER"
+  entity = "allUsers"
+}
+
+resource "google_storage_object_access_control" "image2_access" {
+  bucket = google_storage_bucket.image_bucket.name
+  object = google_storage_bucket_object.image2.name
+  role   = "READER"
+  entity = "allUsers"
+}
