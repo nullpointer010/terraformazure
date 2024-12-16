cf7c9f5f6337502afb3cc897475b6131539a7d1e
nullpointer010 <49768012+nullpointer010@users.noreply.github.com>
2024-12-16 15:27:49 +0100

    Update providers.tf

--- modules/Azure/providers.tf
+++ modules/Azure/providers.tf
@@ -2,9 +2,6 @@ provider "azurerm" {
   features {}
 
   tenant_id       = var.azure_tenant_id
-  client_id       = var.azure_user_name
-  client_secret   = var.azure_password
   subscription_id = var.azure-subscription
 }
-
 
